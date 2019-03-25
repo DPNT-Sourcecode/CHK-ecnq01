433531a79a1aa82cf7d57042441d2aafb7a79737
sbx_user1051 <sbx_user1051@169.254.93.93>
2019-03-25 16:38:38 +0000

    Mon Mar 25 16:38:38 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,8 +7,29 @@ def checkout(skus):
     try:
         values = [a for a in skus]
 
+'''
 
-        dic = {'A':50,'B':30,'C':20,'D':15,'E':40,'F':10}
+| H    | 10    | 5H for 45, 10H for 80  |                     |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        
+'''
+
+        dic = {'A':50,'B':30,'C':20,'D':15,'E':40,'F':10,
+               'G':20,'H':10,'I':35,'J':60}
         re = []
         deals = []
         for key in values:
@@ -59,6 +80,7 @@ def checkout(skus):
         ret = -1
  
     return ret
+
 
 
 
