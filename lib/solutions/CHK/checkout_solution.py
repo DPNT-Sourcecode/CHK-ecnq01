74d510b569872241627eec6a7480c0355b271a9f
sbx_user1051 <sbx_user1051@169.254.93.93>
2019-03-25 14:37:19 +0000

    Mon Mar 25 14:37:19 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,15 +2,21 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+import re
 def checkout(skus):
-    
-    A = 50
-    B = 30
-    C = 20
-    D = 15
-    A_three = 130
-    B_two = 45
+    try:
+        values = re.findall(r'\b\w+\b',skus)
+        A = 50
+        B = 30
+        C = 20
+        D = 15
+        A_three = 130
+        B_two = 45
+        ret = A
+    except:
+        ret = -1
  
-    return -1
+    return ret
+
 
 
