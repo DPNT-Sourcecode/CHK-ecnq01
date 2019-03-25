eba1cbd80c98c8a2e9591d1b7d1fec76a71dbfac
sbx_user1051 <sbx_user1051@169.254.93.93>
2019-03-25 16:44:38 +0000

    Mon Mar 25 16:44:38 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,7 +19,9 @@ def checkout(skus):
 | V    | 50    | 2V for 90, 3V for 130  |                      
 '''
 
-        dic = {'A':50,'B':30,'C':20,'D':15,'E':40,'F':10,
+        dic = {#'A':50,
+               #'B':30,
+               'C':20,'D':15,'E':40,'F':10,
                'G':20,'H':10,'I':35,'J':60,'K':80,'L':90,'M':15,'N':40,
                'O':10,'P':50,'Q':30,'R':50,'S':30,'T':20,'U':40,'V':50,
                'W':20,'X':90,'Y':10,'Z':50}
@@ -33,13 +35,20 @@ def checkout(skus):
         b_deals = deals.count('B')
         e_deals = deals.count('E')
         f_deals = deals.count('F')
+        h_deals = deals.count('H')
+        k_deals = deals.count('K')
+        n_deals = deals.count('N')
+        p_deals = deals.count('P')
+        q_deals = deals.count('Q')
+        r_deals = deals.count('R')
+        u_deals = deals.count('U')
+        v_deals = deals.count('V')
 
 
 #        no_a_deals = a_deals%3
         no_a_deals_fiver = a_deals%5
         
-        no_e_deals = math.floor(e_deals/2)
-
+        '''A'''
         A_three = 0
         
         if no_a_deals_fiver == 0:
@@ -49,7 +58,8 @@ def checkout(skus):
         else:
             A_three = math.floor(a_deals/5)*200 +  math.floor((a_deals - math.floor(a_deals/5)*5)/3) * 130 + math.floor(a_deals - math.floor(a_deals/5)*5 - math.floor((a_deals - math.floor(a_deals/5)*5)/3)*3)*50
         
-        
+        no_e_deals = math.floor(e_deals/2)
+      
         no_b_deals = (b_deals-no_e_deals)%2
         b_deals = max(b_deals - no_e_deals,0)
         if no_b_deals == 0:
@@ -65,11 +75,12 @@ def checkout(skus):
             F_three = math.floor(f_deals/3) *2 * 10 + (f_deals%3)*10
             
             
-        rest = [a for a in re if a != 50 and a != 30 and a != 10]
-        ret = int(sum(rest) + B_two + A_three + F_three)
+#        rest = [a for a in re if a != 50 and a != 30 and a != 10]
+        ret = int(sum(re) + B_two + A_three + F_three)
 
 
     except:
         ret = -1
  
     return ret
+
