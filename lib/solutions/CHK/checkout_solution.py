be96a6e6019749a597762701392c172e87607001
sbx_user1051 <sbx_user1051@169.254.93.93>
2019-03-25 15:48:33 +0000

    Mon Mar 25 15:48:33 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -29,7 +29,8 @@ def checkout(skus):
             A_three = a_deals/3 * 130
 
         else:
-            A_three = math.floor(a_deals/5)*200 + 50* (a_deals%3)
+            A_three = math.floor(a_deals/5)*200 + 
+            math.floor(a_deals - math.floor(a_deals/5)*5) 50* (a_deals%3)
         
         no_b_deals = b_deals%2
         if no_b_deals == 0:
@@ -45,6 +46,7 @@ def checkout(skus):
         ret = -1
  
     return ret
+
 
 
 
