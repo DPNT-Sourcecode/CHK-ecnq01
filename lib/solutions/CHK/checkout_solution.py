8cf006bab4bc372e8e2a0d631c88504dbe27e8e9
sbx_user1051 <sbx_user1051@169.254.93.93>
2019-03-25 14:52:19 +0000

    Mon Mar 25 14:52:19 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,11 +7,14 @@ def checkout(skus):
     try:
         values = re.findall(r'\b\w+\b',skus)
         dic = {'A':50,'B':30,'C':20,'D':15}
+        dic_two = {'C':20,'D':15}
         ret = []
+        ret_two = []
         deals = []
         for key in values:
             print(key)
             ret.append(dic[key])
+            ret_two.append(dic_two[key])
             deals.append(key)
         
         a_deals = deals.count('A')
@@ -29,10 +32,13 @@ def checkout(skus):
         elif b_deals < 2:
             B_two = 0
             
-
-        ret = A
+        if B_two == 0  and A_three == 0:
+            ret = sum(ret)
+        else:
+            ret = sum(ret_two) + B_two + A_three
     except:
         ret = -1
  
     return ret
+
 
