a999cfaae2c7adb97b69aa72653d7958d31e0350
sbx_user1051 <sbx_user1051@169.254.93.93>
2019-03-25 16:47:38 +0000

    Mon Mar 25 16:47:38 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,21 +7,22 @@ def checkout(skus):
     try:
         values = [a for a in skus]
 
-'''
-
-| H    | 10    | 5H for 45, 10H for 80  |                     |
-| K    | 80    | 2K for 150             |                       |
-| N    | 40    | 3N get one M free      |
-| P    | 50    | 5P for 200             |
-| Q    | 30    | 3Q for 80              |
-| R    | 50    | 3R get one Q free      |                    |
-| U    | 40    | 3U get one U free      |
-| V    | 50    | 2V for 90, 3V for 130  |                      
-'''
+        '''
+        
+        | H    | 10    | 5H for 45, 10H for 80  |                     |
+        | K    | 80    | 2K for 150             |                       |
+        | N    | 40    | 3N get one M free      |
+        | P    | 50    | 5P for 200             |
+        | Q    | 30    | 3Q for 80              |
+        | R    | 50    | 3R get one Q free      |                    |
+        | U    | 40    | 3U get one U free      |
+        | V    | 50    | 2V for 90, 3V for 130  |                      
+        '''
 
         dic = {#'A':50,
                #'B':30,
-               'C':20,'D':15,'E':40,'F':10,
+               'C':20,'D':15,'E':40,
+               #'F':10,
                'G':20,'H':10,'I':35,'J':60,'K':80,'L':90,'M':15,'N':40,
                'O':10,'P':50,'Q':30,'R':50,'S':30,'T':20,'U':40,'V':50,
                'W':20,'X':90,'Y':10,'Z':50}
@@ -52,11 +53,23 @@ def checkout(skus):
         A_three = 0
         
         if no_a_deals_fiver == 0:
-            A_three = a_deals/5 * 200
+            A_three = a_deals/10 * 80
+#        elif no_a_deals == 0:
+#            A_three = a_deals/3 * 130
+        else:
+            A_three = math.floor(a_deals/10)*80 +  math.floor((a_deals - math.floor(a_deals/10)*10)/5) * 45 + math.floor(a_deals - math.floor(a_deals/10)*10 - math.floor((a_deals - math.floor(a_deals/10)*10)/5)*5)*10
+
+        '''H'''
+        H_five = 0
+        
+        if no_a_deals_fiver == 0:
+            A_three = a_deals/10 * 200
 #        elif no_a_deals == 0:
 #            A_three = a_deals/3 * 130
         else:
             A_three = math.floor(a_deals/5)*200 +  math.floor((a_deals - math.floor(a_deals/5)*5)/3) * 130 + math.floor(a_deals - math.floor(a_deals/5)*5 - math.floor((a_deals - math.floor(a_deals/5)*5)/3)*3)*50
+        
+
         
         no_e_deals = math.floor(e_deals/2)
       
@@ -83,4 +96,5 @@ def checkout(skus):
         ret = -1
  
     return ret
+
 
