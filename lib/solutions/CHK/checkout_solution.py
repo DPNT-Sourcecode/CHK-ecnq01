3fe678f84484c5c35ca9602d12e83b0d96c92867
sbx_user1051 <sbx_user1051@169.254.93.93>
2019-03-25 16:00:33 +0000

    Mon Mar 25 16:00:33 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -22,17 +22,19 @@ def checkout(skus):
         no_a_deals = a_deals%3
         no_a_deals_fiver = a_deals%5
         
+        no_e_deals = math.floor(e_deals/2)
+
         A_three = 0
         
         if no_a_deals_fiver == 0:
             A_three = a_deals/5 * 200
         elif no_a_deals == 0:
             A_three = a_deals/3 * 130
-
         else:
             A_three = math.floor(a_deals/5)*200 +  math.floor((a_deals - math.floor(a_deals/5)*5)/3) * 130 + math.floor(a_deals - math.floor(a_deals/5)*5 - math.floor((a_deals - math.floor(a_deals/5)*5)/3)*3)*50
         
-        no_b_deals = b_deals%2
+        
+        no_b_deals = (b_deals-no_e_deals)%2
         if no_b_deals == 0:
             B_two = b_deals/2 * 45
         else:
@@ -41,16 +43,8 @@ def checkout(skus):
         rest = [a for a in re if a != 50 and a != 30]
         ret = int(sum(rest) + B_two + A_three)
 
-        no_e_deals = e_deals%2
 
     except:
         ret = -1
  
     return ret
-
-
-
-
-
-
-
